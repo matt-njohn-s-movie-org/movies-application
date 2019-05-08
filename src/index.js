e891b6e19bbb1b911db348213bdeafdf8cc4edc8
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-08 15:12:35 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+import $ from 'jquery';
 sayHello('World');
+
+// on load we want a load screen to appaer
+$("body").append("<div id='loading'><h1> PAGE LOADING</h1></div>");
+$("#loading").css("height", "100%", "width ", "100%");
 
 /**
  * require style imports
@@ -14,7 +19,12 @@ getMovies().then((movies) => {
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
-}).catch((error) => {
+}).then(() => {
+  $('#loading').addClass('invisible')
+}).then(()=>{
+  $('.container').removeClass('invisible')
+} )
+    .catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
